9017d6b188857afe15630bfa753ff91a402bed2c
TseoH <assikevin10@gmail.com>
2022-11-30 22:12:35 +0000

    Update 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for x in range(0, 10):
-        if x == i or i > 0 and x == 0:
+    for x in range(i + 1, 10):
+        if x == i:
             continue
         elif i == 8 and x == 9:
             print('{:d}{:d}'.format(i, x))
