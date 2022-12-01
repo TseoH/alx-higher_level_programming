1698f52523432a6b4504c2f7b9514a18713ee2e4
TseoH <assikevin10@gmail.com>
2022-12-01 21:59:50 +0000

    Add 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
+    import hidden_4
+
+    names = dir(hidden_4)
+    names_len = len(names)
+
+    for i in range(names_len):
+        if names[:2] != '__':
+            print("{}".format([names]))
