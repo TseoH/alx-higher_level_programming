f64baa066b016d0cfd2f0c5210375f31379f088e
TseoH <assikevin10@gmail.com>
2022-12-01 22:16:29 +0000

    Update 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         print("{} arguments.".format(arg_len))
     elif arg_len == 1:
         print("1 argument:")
-        print("1: {}".format(argv[0]))
+        print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(arg_len))
         for i in range(arg_len):
