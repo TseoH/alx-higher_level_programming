1c34c22c042706a96f0696ca46f272f3d96bba91
TseoH <assikevin10@gmail.com>
2023-01-10 23:27:47 +0000

    Add comment

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+"""File reading function"""
+
+
 def read_file(filename=""):
+    """Code here"""
     with open(filename, encoding='utf8') as f:
         print(f.read(), end='')
