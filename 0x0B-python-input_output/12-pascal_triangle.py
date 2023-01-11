9c7b4867209db83134e0c419240451e07308713d
TseoH <assikevin10@gmail.com>
2023-01-11 00:40:08 +0000

    Add pascal_triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""File json function"""
+
+
+def pascal_triangle(n):
+    """Code here"""
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
